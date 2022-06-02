be5aed40dbe9c97bf4ce2d8691a6d97a0144128e
KurumiFake <kurumiisshidowife@gmail.com>
2022-06-02 14:04:28 +0200

    initial

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+rootProject.name = "starlivht"
+
+include("starlivht-api", "starlivht-server")
